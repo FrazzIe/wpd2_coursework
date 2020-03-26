aa33a77ecc010e15dc89c84af59b7f19b30f197c
Fraser Watt <28698343+FrazzIe@users.noreply.github.com>
2020-03-26 20:49:07 +0000

    mysql env vars

--- config.js
+++ config.js
@@ -7,6 +7,13 @@ if (dotenv.error) {
 const config = {
     app: {
         port: process.env.PORT || 3000,
+    },
+    mysql: {
+        connectionLimit: 10,
+        host: process.env.MYSQL_HOST,
+        user: process.env.MYSQL_USER,
+        password: process.env.MYSQL_PASS,
+        database: process.env.MYSQL_DB,
     }
 }
 
