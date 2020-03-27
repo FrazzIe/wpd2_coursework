b4f0caefc9ac9decf0430aedf467ac6a09a75a4a
Fraser Watt <28698343+FrazzIe@users.noreply.github.com>
2020-03-27 02:28:21 +0000

    add basic register logic with password hashing

--- app.js
+++ app.js
@@ -4,7 +4,9 @@ var config = require("./config");
 var mysql = require("./models/mysql");
 var auth = require("./models/auth");
 var passport = require("passport");
-var session = require("express-session"); //import dependencies
+var session = require("express-session");
+var bodyParser = require('body-parser');
+var argon2 = require("argon2"); //import dependencies
 
 var app = express(); //init express app
 
@@ -13,9 +15,11 @@ app.set("view engine", "mustache");
 app.set("views", __dirname + "/public/views"); //setup mustache
 app.use(session({
     secret: config.session.secret,
-}));
+})); //setup session
 app.use(passport.initialize());
-app.use(passport.session());
+app.use(passport.session()); //setup passport session
+app.use(express.static(__dirname + "/public")); //serve static files
+app.use(bodyParser.json()); //parse json requests
 
 app.get("/", function(req, res) {
     res.status(200);
@@ -33,6 +37,30 @@ app.post("/login", passport.authenticate("local", { failureRedirect: "/login" })
 app.get("/logout", function(req, res) {
     req.logout();
     res.redirect("/");
+});
+
+app.post("/register", function(req, res) {
+    console.log(req.body);
+
+    mysql.query(mysql.queries.findUser, [req.body.username, req.body.email]).then((result) => {
+        if (typeof result[0] === "undefined") {
+            argon2.hash(req.body.password).then((hashedPassword) => {
+                mysql.query(mysql.queries.createUser, [req.body.username, req.body.email, hashedPassword]).then((result) => {
+                    res.send("ok");
+                }).catch((error) => {
+                    res.status(500).send(error.message);
+                    console.log((error.message));
+                });
+            }).catch((error) => {
+                res.status(500).send(error.message);
+            });
+        } else {
+            res.send("exists");
+        }
+    }).catch((error) => {
+        console.log((error.message));
+        res.status(500).send(error.message);
+    });
 });
 
 app.listen(config.app.port, () => { //make app listen for port
