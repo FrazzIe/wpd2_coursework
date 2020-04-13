ff956ed357b0935aa7055131678b35c21cdb7d3b
Fraser Watt <28698343+FrazzIe@users.noreply.github.com>
2020-04-13 11:16:51 +0100

    remove debug msg

--- models/mailer.js
+++ models/mailer.js
@@ -1,7 +1,6 @@
 var config = require("./../config");
 var nodemailer = require("nodemailer");
 
-console.log(config.mailer.service, config.mailer.user, config.mailer.password);
 var transporter = nodemailer.createTransport({
     service: config.mailer.service,
     auth: {
